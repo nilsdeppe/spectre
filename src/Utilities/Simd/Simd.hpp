f52867db5c88c102fbabfaf7d678319ddf584a24
Nils Deppe <nils@nilsdeppe.com>
2022-08-02 06:44:16 -0700

    Add simd library wrapping xsimd
    The goal is again to wrap the external library so that we are somewhat agnostic
    to the specific simd library we are using.

---
+++ src/Utilities/Simd/Simd.hpp
@@ -0,0 +1,33 @@
+// Distributed under the MIT License.
+// See LICENSE.txt for details.
+
+#pragma once
+
+#ifdef SPECTRE_USE_XSIMD
+#include <xsimd/xsimd.hpp>
+
+#include "Utilities/TypeTraits/CreateGetTypeAliasOrDefault.hpp"
+
+/// Namespace containing SIMD functions based on XSIMD.
+namespace simd = xsimd;
+
+namespace MakeWithValueImpls {
+template <typename U, typename T, typename Arch>
+struct MakeWithValueImpl<xsimd::batch<U, Arch>, T> {
+  static SPECTRE_ALWAYS_INLINE xsimd::batch<U, Arch> apply(const T& /* input */,
+                                                           const U value) {
+    return xsimd::batch<U, Arch>(value);
+  }
+};
+}  // namespace MakeWithValueImpls
+
+namespace xsimd {
+inline bool any(const bool t) { return t; }
+inline bool all(const bool t) { return t; }
+inline bool none(const bool t) { return not t; }
+
+CREATE_GET_TYPE_ALIAS_OR_DEFAULT(value_type)
+
+
+}  // namespace xsimd
+#endif
