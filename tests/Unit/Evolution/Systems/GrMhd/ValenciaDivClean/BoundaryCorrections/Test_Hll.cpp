0e52ad4231cf67e523f0e5a211e0b47628a0253e
Nils Deppe <nils@nilsdeppe.com>
2023-12-20 19:55:57 -0800

    Use hydro speeds in GRMHD HLL where magnetic field is zero

--- tests/Unit/Evolution/Systems/GrMhd/ValenciaDivClean/BoundaryCorrections/Test_Hll.cpp
+++ tests/Unit/Evolution/Systems/GrMhd/ValenciaDivClean/BoundaryCorrections/Test_Hll.cpp
@@ -16,10 +16,35 @@
 #include "NumericalAlgorithms/Spectral/Basis.hpp"
 #include "NumericalAlgorithms/Spectral/Mesh.hpp"
 #include "NumericalAlgorithms/Spectral/Quadrature.hpp"
+#include "PointwiseFunctions/Hydro/EquationsOfState/EquationOfState.hpp"
+#include "PointwiseFunctions/Hydro/EquationsOfState/PolytropicFluid.hpp"
+#include "PointwiseFunctions/Hydro/Tags.hpp"
 #include "Utilities/TaggedTuple.hpp"
 
 namespace {
 namespace helpers = TestHelpers::evolution::dg;
+
+struct ConvertPolytropic {
+  using unpacked_container = bool;
+  using packed_container = EquationsOfState::EquationOfState<true, 3>;
+  using packed_type = bool;
+
+  static inline unpacked_container unpack(const packed_container& /*packed*/,
+                                          const size_t /*grid_point_index*/) {
+    return true;
+  }
+
+  [[noreturn]] static inline void pack(
+      const gsl::not_null<packed_container*> /*packed*/,
+      const unpacked_container& /*unpacked*/,
+      const size_t /*grid_point_index*/) {
+    ERROR("Should not be converting an EOS from an unpacked to a packed type");
+  }
+
+  static inline size_t get_size(const packed_container& /*packed*/) {
+    return 1;
+  }
+};
 
 SPECTRE_TEST_CASE("Unit.GrMhd.ValenciaDivClean.BoundaryCorrections.Hll",
                   "[Unit][GrMhd]") {
@@ -34,28 +59,32 @@ SPECTRE_TEST_CASE("Unit.GrMhd.ValenciaDivClean.BoundaryCorrections.Hll",
       helpers::Tags::Range<gr::Tags::Lapse<DataVector>>,
       helpers::Tags::Range<gr::Tags::Shift<DataVector, 3>>>
       ranges{std::array{0.3, 1.0}, std::array{0.01, 0.02}};
+  const tuples::TaggedTuple<hydro::Tags::GrmhdEquationOfState> volume_data{
+      EquationsOfState::PolytropicFluid<true>{100.0, 2.0}.promote_to_3d_eos()};
 
   TestHelpers::evolution::dg::test_boundary_correction_conservation<system>(
       make_not_null(&gen), grmhd::ValenciaDivClean::BoundaryCorrections::Hll{},
-      Mesh<2>{5, Spectral::Basis::Legendre, Spectral::Quadrature::Gauss}, {},
-      ranges);
+      Mesh<2>{5, Spectral::Basis::Legendre, Spectral::Quadrature::Gauss},
+      volume_data, ranges);
 
-  TestHelpers::evolution::dg::test_boundary_correction_with_python<system>(
+  TestHelpers::evolution::dg::test_boundary_correction_with_python<
+      system, tmpl::list<ConvertPolytropic>>(
       make_not_null(&gen), "Hll", "dg_package_data", "dg_boundary_terms",
       grmhd::ValenciaDivClean::BoundaryCorrections::Hll{},
-      Mesh<2>{5, Spectral::Basis::Legendre, Spectral::Quadrature::Gauss}, {},
-      ranges);
+      Mesh<2>{5, Spectral::Basis::Legendre, Spectral::Quadrature::Gauss},
+      volume_data, ranges);
 
   const auto hll = TestHelpers::test_creation<std::unique_ptr<
       grmhd::ValenciaDivClean::BoundaryCorrections::BoundaryCorrection>>(
       "Hll:");
 
-  TestHelpers::evolution::dg::test_boundary_correction_with_python<system>(
+  TestHelpers::evolution::dg::test_boundary_correction_with_python<
+      system, tmpl::list<ConvertPolytropic>>(
       make_not_null(&gen), "Hll", "dg_package_data", "dg_boundary_terms",
       dynamic_cast<const grmhd::ValenciaDivClean::BoundaryCorrections::Hll&>(
           *hll),
-      Mesh<2>{5, Spectral::Basis::Legendre, Spectral::Quadrature::Gauss}, {},
-      ranges);
+      Mesh<2>{5, Spectral::Basis::Legendre, Spectral::Quadrature::Gauss},
+      volume_data, ranges);
 
   CHECK_FALSE(grmhd::ValenciaDivClean::BoundaryCorrections::Hll{} !=
               grmhd::ValenciaDivClean::BoundaryCorrections::Hll{});
